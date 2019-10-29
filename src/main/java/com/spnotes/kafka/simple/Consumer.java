e249118e5c622fe0565fa83d595e313653bdfb55
Mallikarjuna Bodepudi <bmallikarjuna66@gmail.com>
2019-10-28 21:00:01 -0500

    updated the  code

--- src/main/java/com/spnotes/kafka/simple/Consumer.java
+++ src/main/java/com/spnotes/kafka/simple/Consumer.java
@@ -11,7 +11,7 @@ import java.util.Properties;
 import java.util.Scanner;
 
 /**
- * Created by sunilpatil on 12/28/15.
+ * Created by Arjun on 10/28/19
  */
 public class Consumer {
     private static Scanner in;
--- src/main/java/com/spnotes/kafka/simple/Producer.java
+++ src/main/java/com/spnotes/kafka/simple/Producer.java
@@ -8,34 +8,53 @@ import java.util.Properties;
 import java.util.Scanner;
 
 /**
- * Created by sunilpatil on 12/28/15.
+ * Created by Arjun on 10/28/2019
  */
 public class Producer {
+
     private static Scanner in;
-    public static void main(String[] argv)throws Exception {
+
+    public static void main(String[] argv) throws Exception {
         if (argv.length != 1) {
             System.err.println("Please specify 1 parameters ");
             System.exit(-1);
         }
         String topicName = argv[0];
         in = new Scanner(System.in);
+        System.out.println("-------------------------------");
+        System.out.println("Let's Generate random string");
+        System.out.println("-------------------------------");
         System.out.println("Enter message(type exit to quit)");
-
+        
         //Configure the Producer
         Properties configProperties = new Properties();
-        configProperties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"localhost:9092");
-        configProperties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,"org.apache.kafka.common.serialization.ByteArraySerializer");
-        configProperties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,"org.apache.kafka.common.serialization.StringSerializer");
+        configProperties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        configProperties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.ByteArraySerializer");
+        configProperties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
 
         org.apache.kafka.clients.producer.Producer producer = new KafkaProducer(configProperties);
-        String line = in.nextLine();
-        while(!line.equals("exit")) {
+       int counter = 1;
+        while (counter < 11) {
             //TODO: Make sure to use the ProducerRecord constructor that does not take parition Id
-            ProducerRecord<String, String> rec = new ProducerRecord<String, String>(topicName,line);
+            String str =  randomAlphaNumeric(counter);
+            System.out.println("Length"+counter+"\nGenerated String : " + str);
+            ProducerRecord<String, String> rec = new ProducerRecord<String, String>(topicName, str);
             producer.send(rec);
-            line = in.nextLine();
+           counter++;
         }
         in.close();
         producer.close();
+    }
+
+    private static final String ALPHA_NUMERIC_STRING = "1234 GEt on the dance floor, I like this Song";
+
+    public static String randomAlphaNumeric(int count) {
+      
+        StringBuilder builder = new StringBuilder();
+        while (count-- != 0) {
+            int character = (int) (Math.random() * ALPHA_NUMERIC_STRING.length());
+            builder.append(ALPHA_NUMERIC_STRING.charAt(character));
+        }
+        return builder.toString();
     }
 }
